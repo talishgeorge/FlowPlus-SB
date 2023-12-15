880bb321a7dc141c95719dffb0cd66669bc680d8
Talish George <talishtest@gmail.com>
2023-12-15 14:38:17 +1100

    Generate docs and refactored to small functions

--- FlowPlus-SB/SceneDelegate.swift
+++ FlowPlus-SB/SceneDelegate.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
     var window: UIWindow?
-    
+
     let navBarAppearance = createNavBarAppearance()
     let tabBarAppearance = createTabBarAppearance()
-    
+
+    /**
+     This method is called when a scene is about to be connected to a session.
+     It sets up the tab bar controller and assigns it as the root view controller of the window.
+     - Parameters:
+        - scene: The scene that is about to be connected.
+        - session: The session associated with the scene.
+        - options: The connection options for the scene.
+     */
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let tabController = setupTabBarController()
@@ -23,53 +29,101 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.makeKeyAndVisible()
         self.window = window
     }
-    
+
+    /**
+     Sets up the tab bar controller with the appropriate view controllers and appearance.
+     - Returns: The configured tab bar controller.
+     */
     func setupTabBarController() -> UITabBarController {
+        
         let tabController = UITabBarController()
         tabController.tabBar.standardAppearance = tabBarAppearance
         if #available(iOS 13.0, *) {
             tabController.tabBar.scrollEdgeAppearance = tabBarAppearance
         }
-        
-        let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
-        let searchStoryboard = UIStoryboard(name: "Search", bundle: nil)
-        let newPostStoryboard = UIStoryboard(name: "NewPost", bundle: nil)
-        let profileStoryboard = UIStoryboard(name: "Profile", bundle: nil)
-        let activityStoryboard = UIStoryboard(name: "Activity", bundle: nil)
-        
-        let homeVC = homeStoryboard.instantiateViewController(withIdentifier: "Home") as! HomeViewController
-        let profileVC = profileStoryboard.instantiateViewController(withIdentifier: "Profile") as! ProfileViewController
-        let searchVC = searchStoryboard.instantiateViewController(withIdentifier: "Search") as! SearchViewController
-        let newPostVC = newPostStoryboard.instantiateViewController(withIdentifier: "NewPost") as! NewPostViewController
-        let activityVC = activityStoryboard.instantiateViewController(withIdentifier: "Activity") as! ActiivityViewController
-        
+
         let vcData: [(UIViewController, UIImage?, UIImage?)] = [
-            (homeVC, UIImage(named: "home_tab_icon"), UIImage(named: "home_selected_tab_icon")),
-            (searchVC, UIImage(named: "search_tab_icon"), UIImage(named: "search_selected_tab_icon")),
-            (newPostVC, UIImage(named: "post_tab_icon"), UIImage(named: "post_tab_icon")),
-            (activityVC, UIImage(named: "activity_tab_icon"), UIImage(named: "activity_selected_tab_icon")),
-            (profileVC, UIImage(named: "profile_tab_icon"), UIImage(named: "profile_selected_tab_icon"))
+            (createHomeVC(), UIImage(named: "home_tab_icon"), UIImage(named: "home_selected_tab_icon")),
+            (createSearchVC(), UIImage(named: "search_tab_icon"), UIImage(named: "search_selected_tab_icon")),
+            (createNewPostVC(), UIImage(named: "post_tab_icon"), UIImage(named: "post_tab_icon")),
+            (createActivityVC(), UIImage(named: "activity_tab_icon"), UIImage(named: "activity_selected_tab_icon")),
+            (createProfileVC(), UIImage(named: "profile_tab_icon"), UIImage(named: "profile_selected_tab_icon")),
         ]
-        
-        let vcs = vcData.map { vc, defaultImage, selectedImage -> UINavigationController in
-            let nav = UINavigationController(rootViewController: vc)
-            if let navBarAppearance = navBarAppearance {
-                nav.navigationBar.standardAppearance = navBarAppearance
-                if #available(iOS 13.0, *) {
-                    nav.navigationBar.scrollEdgeAppearance = navBarAppearance
-                }
-            }
-            
-            nav.tabBarItem.image = defaultImage?.withRenderingMode(.alwaysOriginal)
-            nav.tabBarItem.selectedImage = selectedImage?.withRenderingMode(.alwaysOriginal)
-            return nav
-        }
-        
+
+        let vcs = vcData.map { createNavController(rootVC: $0.0, defaultImage: $0.1, selectedImage: $0.2) }
+
         tabController.viewControllers = vcs
         tabController.tabBar.isTranslucent = false
-        
+
         return tabController
     }
+
+    /**
+     Creates a navigation controller with the specified root view controller and appearance.
+     - Parameters:
+        - rootVC: The root view controller of the navigation controller.
+        - defaultImage: The default image for the tab bar item.
+        - selectedImage: The selected image for the tab bar item.
+     - Returns: The configured navigation controller.
+     */
+    func createNavController(rootVC: UIViewController, defaultImage: UIImage?, selectedImage: UIImage?) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootVC)
+        if let navBarAppearance = navBarAppearance {
+            nav.navigationBar.standardAppearance = navBarAppearance
+            if #available(iOS 13.0, *) {
+                nav.navigationBar.scrollEdgeAppearance = navBarAppearance
+            }
+        }
+        nav.tabBarItem.image = defaultImage?.withRenderingMode(.alwaysOriginal)
+        nav.tabBarItem.selectedImage = selectedImage?.withRenderingMode(.alwaysOriginal)
+        return nav
+    }
+
+    /**
+     Creates a home view controller from the "Home" storyboard.
+     - Returns: The created home view controller.
+     */
+    func createHomeVC() -> HomeViewController {
+        let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
+        return homeStoryboard.instantiateViewController(withIdentifier: "Home") as! HomeViewController
+    }
+
+    /**
+     Creates a search view controller from the "Search" storyboard.
+     - Returns: The created search view controller.
+     */
+    func createSearchVC() -> SearchViewController {
+        let searchStoryboard = UIStoryboard(name: "Search", bundle: nil)
+        return searchStoryboard.instantiateViewController(withIdentifier: "Search") as! SearchViewController
+    }
+
+    /**
+     Creates a new post view controller from the "NewPost" storyboard.
+     - Returns: The created new post view controller.
+     */
+    func createNewPostVC() -> NewPostViewController {
+        let newPostStoryboard = UIStoryboard(name: "NewPost", bundle: nil)
+        return newPostStoryboard.instantiateViewController(withIdentifier: "NewPost") as! NewPostViewController
+    }
+
+    /**
+     Creates an activity view controller from the "Activity" storyboard.
+     - Returns: The created activity view controller.
+     */
+    func createActivityVC() -> ActiivityViewController {
+        let activityStoryboard = UIStoryboard(name: "Activity", bundle: nil)
+        return activityStoryboard.instantiateViewController(withIdentifier: "Activity") as! ActiivityViewController
+    }
+
+    /**
+     Creates a profile view controller from the "Profile" storyboard.
+     - Returns: The created profile view controller.
+     */
+    func createProfileVC() -> ProfileViewController {
+        let profileStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+        return profileStoryboard.instantiateViewController(withIdentifier: "Profile") as! ProfileViewController
+    }
+
     /// Creates a customized navigation bar appearance.
     /// - Returns: The customized UINavigationBarAppearance object.
     static func createNavBarAppearance() -> UINavigationBarAppearance? {
@@ -79,16 +133,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         UINavigationBar.appearance().tintColor = .systemGreen
         return appearance
     }
-    
-    /// Creates a customized appearance for the tab bar.
-    /// - Returns: A `UITabBarAppearance` object with the customized appearance.
+
+    /// Creates a customized tab bar appearance.
+    /// - Returns: The customized UITabBarAppearance object.
     static func createTabBarAppearance() -> UITabBarAppearance {
         let tabBarAppearance = UITabBarAppearance()
         tabBarAppearance.configureWithOpaqueBackground()
         tabBarAppearance.backgroundColor = .systemGray6
         return tabBarAppearance
     }
-    
+
+    /// Sets up a customized navigation bar appearance.
+    /// - Returns: The customized UINavigationBarAppearance object.
     func setupNavBarAppearance() -> UINavigationBarAppearance {
         let coloredAppearance = UINavigationBarAppearance()
         coloredAppearance.configureWithOpaqueBackground()
@@ -97,36 +153,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         return coloredAppearance
     }
-    
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-    
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-    
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-    
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-    
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-        
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-    }
 }
-
